863e2cee58c23600f4b657f9d2bb922eccfd3644
m.asif <m.asif@arbisoft.com>
2022-08-07 20:16:51 +0500

    renamge file and changes

--- git_project/login.py
+++ git_project/login.py
@@ -4,3 +4,6 @@ from git_project.index import Display
 class Login(Display):
     def display_data(self):
         print(self.display_data())
+
+    def display_remove(self):
+        print(self.display_data())
