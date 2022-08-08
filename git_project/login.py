1efe741abbd15656732f47487f7bb0d4cc13dc7e
m.asif <m.asif@arbisoft.com>
2022-08-08 18:27:47 +0500

    add call of disk full funciton

--- git_project/login.py
+++ git_project/login.py
@@ -7,6 +7,9 @@ class Login(Display):
         print(self.display_data())
     def get_user_inof(self):
         print('user info')
+        if diskfull():
+            print('disk is fulll')
+
 
 object = Login()
 object.display_remove()
