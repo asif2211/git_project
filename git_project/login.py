bef4ab9c298f7da0c04ab919020c3bda05d4802c
m.asif <m.asif@arbisoft.com>
2022-08-08 18:47:17 +0500

    added data

--- git_project/login.py
+++ git_project/login.py
@@ -5,12 +5,10 @@ class Login(Display):
 
     def display_remove(self):
         print(self.display_data())
-
     def get_user_inof(self):
         print('user info')
         if diskfull():
-            print('disk is fulll')
-
+            print(d'disk full')
 
 object = Login()
 object.display_remove()
