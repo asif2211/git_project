a4a5dbcfbcc8fc4c4da943a4ee919dab8ea78b31
m.asif <m.asif@arbisoft.com>
2022-08-10 13:51:36 +0500

    create new_branch and add files into int

--- git_project/get_information.py
+++ git_project/get_information.py
@@ -1,9 +1,6 @@
 
-class GetInformation:
+class Getinfo:
     def get_pc_info(self):
         print('get pc info')
 
-class GetInfo:
-    def get_info_of_pc(self):
-        print('get info of our pc ')
 
--- git_project/locator.py
+++
@@ -1,9 +0,0 @@
-class Display :
-    def get_date(self):
-        return "HELLO THIS IS DIPALY DATA"
-
-    def display_value(self):
-        print('data')
-
-    def get_value(self):
-        return "HELLO THIS IS DIPALY DATA"
